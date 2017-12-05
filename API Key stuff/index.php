0b83f68b641ef397ab062085ef188530b5005bcf
JФSЭРНI KRДКФШSKI﻿ <yeehawlerz101@gmail.com>
2017-12-05 15:00:24 -0500

    Update

--- API Key stuff/index.php
+++ API Key stuff/index.php
@@ -97,7 +97,7 @@
         "api_key" => "ttxcPt83bTJO",
         "start_url" => "http://turfwarsuk.com/temp/",
         "start_template" => "main_template",
-        "start_value_override" => "{\"query\": \"San Francisco\"}",
+        "start_value_override" => "{\"API-Request\": \"Recived-From-Web\"}",
         "send_email" => "1"
       );
 
